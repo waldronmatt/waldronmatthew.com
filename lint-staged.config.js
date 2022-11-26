70f4200b0cd0d614dfd85371979cde881f06d1f5
waldronmatt <mhuntwaldron@gmail.com>
2022-11-26 18:06:00 -0500

    fix(global): remove/update configs

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,13 @@
 const config = require('@waldronmatt/lint-staged-config');
-const ymlConfig = require('@waldronmatt/lint-staged-config');
 
 module.exports = {
-  '*': ['secretlint'],
+  // ignore prettier on unknown extensions
   '!(*.{md,js,jsx,ts,tsx,json,css,scss,yml,yaml})': [
     'prettier --cache --write --ignore-unknown',
   ],
-  ...ymlConfig,
   ...config,
+  // lint and fix changed markdown files
+  '*.md': ['prettier --cache --write', 'markdownlint'],
+  // lint and fix changed css and scss files
+  '*.{css,scss}': ['prettier --cache --write', 'stylelint --cache --fix'],
 };
