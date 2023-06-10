3b8de563b2269d8911d93e47b7e0838dd97dbfdb
waldronmatt <mhuntwaldron@gmail.com>
2023-06-10 18:57:45 -0400

    fix(global): lazy load translation library when clicked

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'cache7'; // Change value to force update
+const cacheName = 'cache8'; // Change value to force update
 
 self.addEventListener('install', event => {
   // Kick out the old service worker
--- src/js/translation.js
+++ src/js/translation.js
@@ -1,34 +1,6 @@
 import Translator from '@andreasremdt/simple-translator';
 
-const translator = new Translator({
+// eslint-disable-next-line import/prefer-default-export
+export const translator = new Translator({
   defaultLanguage: 'en',
-});
-
-translator
-  .add('es', {
-    description: 'Desarrollador Sénior',
-    keywords: 'full stack, desarrollador, sénior',
-    language: 'English',
-    resume: 'Currículum',
-    cv: 'Currículum vítae',
-    contact: 'Contacto',
-    senior: 'desarrollador',
-    developer: 'sénior',
-  })
-  .add('en', {
-    description: 'Senior Developer',
-    keywords: 'full stack, development, senior',
-    language: 'Español',
-    resume: 'Resume',
-    cv: 'Curriculum vitae',
-    contact: 'Contact',
-    senior: 'senior',
-    developer: 'developer',
-  });
-
-document.querySelector('.language-toggle')?.addEventListener('click', () => {
-  // eslint-disable-next-line no-unused-expressions
-  document.documentElement.lang === 'en'
-    ? translator.translatePageTo('es')
-    : translator.translatePageTo('en');
 });
